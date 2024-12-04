1c47db35b0f2cc7b327a79c5375567c07fd58bda
DaviHVL <davihvl@gmail.com>
2024-12-04 15:38:16 -0300

    feat(feed-deslogado): adição dos icones clicaveis dos professores

--- src/app/feed-deslogado/page.tsx
+++ src/app/feed-deslogado/page.tsx
@@ -1,6 +1,7 @@
 import HeaderDeslogado from "../components/HeaderDeslogado";
 import Image from "next/image";
 import Lupa from "../../../public/imagens/lupa.svg";
+import Professor from "../../../public/imagens/professor.svg";
 
 const perfilDeslogado = () => {
   return (
@@ -17,11 +18,40 @@ const perfilDeslogado = () => {
                 <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
                     <Image src={Lupa} alt="Ícone de Busca" width={25} height={25} />
                 </div> 
-
-           </div>
-
-           
+           </div>   
         </div>
+
+        <div className="flex items-center justify-center w-full p-4 rounded-lg bg-white">
+            <div className= "w-[236px] h-[281px] mx-10 mt-5 bg-gray-100 rounded-lg shadow-md flex flex-col items-center justify-start p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+                <Image src={Professor} alt="Imagem do Professor" className="w-[185px] h-[185px] object-cover"/>
+                <h2 className="text-lg font-medium text-gray-800">Nome</h2>
+                <h3 className="text-sm text-gray-500">Disciplina</h3>
+            </div>
+
+            <div className= "w-[236px] h-[281px] mx-10 mt-5 bg-gray-100 rounded-lg shadow-md flex flex-col items-center justify-start p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+                <Image src={Professor} alt="Imagem do Professor" className="w-[185px] h-[185px] object-cover"/>
+                <h2 className="text-lg font-medium text-gray-800">Nome</h2>
+                <h3 className="text-sm text-gray-500">Disciplina</h3>
+            </div>
+
+            <div className= "w-[236px] h-[281px] mx-10 mt-5 bg-gray-100 rounded-lg shadow-md flex flex-col items-center justify-start p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+                <Image src={Professor} alt="Imagem do Professor" className="w-[185px] h-[185px] object-cover"/>
+                <h2 className="text-lg font-medium text-gray-800">Nome</h2>
+                <h3 className="text-sm text-gray-500">Disciplina</h3>
+            </div>
+
+            <div className= "w-[236px] h-[281px] mx-10 mt-5 bg-gray-100 rounded-lg shadow-md flex flex-col items-center justify-start p-4 cursor-pointer transform transition-transform duration-300 hover:scale-105">
+                <Image src={Professor} alt="Imagem do Professor" className="w-[185px] h-[185px] object-cover"/>
+                <h2 className="text-lg font-medium text-gray-800">Nome</h2>
+                <h3 className="text-sm text-gray-500">Disciplina</h3>
+            </div>
+        </div>
+
+        <div className="flex justify-center items-center">
+            <div className="w-[1184px] h-[4px] bg-black mt-8"></div>
+        </div>
+
+        
 
     
         
