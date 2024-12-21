96a8eb6692381d23ef934388be3ca39a442304b5
Mateus Lázaro <mundimlazaro@gmail.com>
2024-12-20 21:07:39 -0300

    "o modal tava hardcoded ainda"

--- src/app/components/ModalPerfil/ModalEditarPerfil.tsx
+++ src/app/components/ModalPerfil/ModalEditarPerfil.tsx
@@ -8,53 +8,57 @@ import fotoPerfil from "../../../../public/imagens/perfil.png";
 interface ModalEditarPerfilProps {
   isOpen: boolean;
   onClose: () => void;
+  usuarioId: string; // ID do usuário ou professor
+  tipoPerfil: "user" | "professor"; // Define se é usuário ou professor
 }
 
-interface usuarioPerfilProps {
+interface UsuarioPerfilProps {
   nome: string;
   email: string;
-  curso: string;
-  departamento: string;
+  curso?: string;
+  departamento?: string;
   senha: string;
 }
 
 const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
   isOpen,
   onClose,
+  usuarioId,
+  tipoPerfil,
 }) => {
-  const [usuario, setUsuario] = useState<usuarioPerfilProps | null>(null);
+  const [usuario, setUsuario] = useState<UsuarioPerfilProps | null>(null);
   const [senhaAtual, setSenhaAtual] = useState("");
   const [novaSenha, setNovaSenha] = useState("");
   const [confirmarNovaSenha, setConfirmarNovaSenha] = useState("");
   const [erroSenhaAtual, setErroSenhaAtual] = useState("");
   const [erroNovaSenha, setErroNovaSenha] = useState("");
 
-    const usuarioId = 3;
-
   useEffect(() => {
     const fetchUsuarioData = async () => {
       try {
-        const response = await fetch(`http://localhost:3000/user/${usuarioId}`);
-        console.log("Status da resposta:", response.status);
+        const endpoint =
+          tipoPerfil === "user"
+            ? `http://localhost:3000/user/${usuarioId}`
+            : `http://localhost:3000/professor/${usuarioId}`;
+        const response = await fetch(endpoint);
 
         if (!response.ok) {
           throw new Error("Falha na requisição da API");
         }
 
         const data = await response.json();
+        if (data) {
+          setUsuario(data);
+        } else {
+          throw new Error("Dados inválidos recebidos");
+        }
+      } catch (error) {
+        console.error("Erro ao carregar dados do perfil", error);
+      }
+    };
 
-                if (data) {
-                    setUsuario(data);
-                } else {
-                    throw new Error('Dados inválidos recebidos');
-                }
-            } catch (error) {
-                console.error('Erro ao carregar dados do perfil', error);
-            }
-        };
-
-    fetchUsuarioData();
-  }, [usuarioId]);
+    if (usuarioId) fetchUsuarioData();
+  }, [usuarioId, tipoPerfil]);
 
   const validarSenhaAtual = () => {
     if (!usuario) return;
@@ -76,13 +80,10 @@ const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    if (!usuario) return;
+
     let erroAtual = "";
     let erroNova = "";
-
-    if (!usuario) {
-      console.log("Erro: Usuário não carregado.");
-      return;
-    }
 
     if (senhaAtual !== usuario.senha) {
       erroAtual = "A senha atual está incorreta";
@@ -95,7 +96,6 @@ const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
     if (erroAtual || erroNova) {
       setErroSenhaAtual(erroAtual);
       setErroNovaSenha(erroNova);
-      console.log("Senha inválida. Requisição recusada.");
       return;
     }
 
@@ -115,7 +115,11 @@ const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
     };
 
     try {
-      const response = await fetch(`http://localhost:3000/user/${usuarioId}`, {
+      const endpoint =
+        tipoPerfil === "user"
+          ? `http://localhost:3000/user/${usuarioId}`
+          : `http://localhost:3000/professor/${usuarioId}`;
+      const response = await fetch(endpoint, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
@@ -149,7 +153,7 @@ const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
 
   return (
     <Dialog open={isOpen} onClose={onClose} className="relative z-50">
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <div className="fixed inset-0 bg-black/30" />
       <div className="fixed inset-0 flex items-center justify-center p-4">
         <Dialog.Panel className="w-full max-w-md p-6 bg-background rounded-lg shadow-lg">
           <Image
@@ -168,82 +172,39 @@ const ModalEditarPerfil: React.FC<ModalEditarPerfilProps> = ({
                 className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
               />
             </div>
-            <div className="mt-4">
-              <input
-                type="email"
-                name="email"
-                placeholder="Email"
-                defaultValue={usuario.email}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-                readOnly
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                type="text"
-                name="curso"
-                placeholder="Curso"
-                defaultValue={usuario.curso}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                type="text"
-                name="departamento"
-                placeholder="Departamento"
-                defaultValue={usuario.departamento}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                type="password"
-                placeholder="Senha atual"
-                value={senhaAtual}
-                onChange={(e) => setSenhaAtual(e.target.value)}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-                onBlur={validarSenhaAtual}
-              />
-              {erroSenhaAtual && (
-                <p className="text-darkRed text-sm">{erroSenhaAtual}</p>
-              )}
-            </div>
-            <div className="mt-4">
-              <input
-                type="password"
-                placeholder="Nova senha"
-                value={novaSenha}
-                onChange={(e) => setNovaSenha(e.target.value)}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-              />
-            </div>
-            <div className="mt-4">
-              <input
-                type="password"
-                placeholder="Confirmar nova senha"
-                value={confirmarNovaSenha}
-                onChange={(e) => setConfirmarNovaSenha(e.target.value)}
-                className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
-                onBlur={validarNovaSenha}
-              />
-              {erroNovaSenha && (
-                <p className="text-darkRed text-sm">{erroNovaSenha}</p>
-              )}
-            </div>
+            {tipoPerfil === "user" && (
+              <div className="mt-4">
+                <input
+                  type="text"
+                  name="curso"
+                  placeholder="Curso"
+                  defaultValue={usuario.curso}
+                  className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
+                />
+              </div>
+            )}
+            {tipoPerfil === "professor" && (
+              <div className="mt-4">
+                <input
+                  type="text"
+                  name="departamento"
+                  placeholder="Departamento"
+                  defaultValue={usuario.departamento}
+                  className="w-full px-3 py-2 border rounded-[20px] focus:outline-none focus:ring"
+                />
+              </div>
+            )}
             <div className="mt-6 flex justify-center gap-2">
               <Button
                 type="button"
                 onClick={onClose}
-                className={`bg-gray-200 rounded-full ${inter400.className} text-gray-700 border-2 w-36 h-9
-                                 border-darkBlue hover:shadow-inner hover:shadow-gray-500`}
+                className={`bg-gray-200 rounded-full ${inter400.className} text-gray-700 border-2 w-36 h-9`}
               >
                 Cancelar
               </Button>
               <Button
                 type="submit"
-                className={`bg-lightGreen rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9
-                                 border-darkBlue hover:shadow-inner hover:shadow-green-400`}
+                className={`bg-lightGreen rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9`}
               >
                 Salvar
               </Button>
