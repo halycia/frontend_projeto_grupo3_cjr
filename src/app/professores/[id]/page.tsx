0cf4332e6aa0724cbbf017f7b2c28c041efe50f4
lbritors <britoleticiars@gmail.com>
2024-12-20 21:55:42 -0300

    feat: passar id e renderizar header codicionalmentel

--- src/app/professores/[id]/page.tsx
+++ src/app/professores/[id]/page.tsx
@@ -9,17 +9,20 @@ import Link from "next/link";
 import Header from "../../components/Header";
 import { inter400, inter700, inter800 } from "../../fonts/fonts";
 import "../../globals.css";
-import fotoPerfil from "../../../../public/imagens/perfil.png";
+import professorFoto from "../../../../public/imagens/rick.svg";
 import Publicacao from "../../components/Publicacao";
 import ModalEditarPerfil from "../../components/ModalPerfil/ModalEditarPerfil";
 import { CircleArrowLeft } from "lucide-react";
 import api from "@/utils/api";
 import { useParams } from "next/navigation";
+import { useAuth } from "@/app/context/authContext";
+import HeaderDeslogado from "@/app/components/HeaderDeslogado";
 
 export default function PerfilLogadoPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [avaliacao, setAvaliacao] = useState<any[]>([]);
   const [professor, setProfessor] = useState<any>();
+  const { userId, isAuthenticated } = useAuth();
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
   const { id } = useParams();
@@ -38,8 +41,8 @@ export default function PerfilLogadoPage() {
     }
     try {
       if (id) {
-        const response = await api.get(`avaliacoes/professor/${Number(id)}`);
-        const professor = await api.get(`professores/${Number(id)}`);
+        const response = await api.get(`avaliacoes/professor/${id}`);
+        const professor = await api.get(`professores/${id}`);
         console.log(response.data);
         console.log(professor.data);
         setAvaliacao(response.data);
@@ -52,7 +55,7 @@ export default function PerfilLogadoPage() {
 
   return (
     <div className="bg-background flex flex-col justify-center items-center h-full w-screen relative">
-      <Header />
+      {isAuthenticated ? <Header /> : <HeaderDeslogado />}
       <div className="flex items-center w-full h-[30px]">
         <Button className="focus:outline-none rounded-full hover:bg-emerald-300 ml-20">
           <Link href={"feed-logado"}>
@@ -64,37 +67,14 @@ export default function PerfilLogadoPage() {
         <div className="h-auto w-full">
           <div className="h-[151px] bg-darkGreen shadow shadow-gray-500 flex relative">
             <Image
-              src={fotoPerfil}
+              src={professorFoto}
               alt="foto-perfil"
-              className="rounded-full w-36 h-36 absolute left-12 top-16 object-cover sm:w-18 sm:h-18"
+              className="rounded-full w-32 h-32  absolute left-12 top-16 object-cover sm:w-18 sm:h-18"
             />
           </div>
           <div
-            className={`bg-white h-auto border-solid border-b-2 w-full flex flex-col justify-around ${inter700.className}`}
+            className={`bg-white mt-12 h-auto border-solid border-b-2 w-full flex flex-col justify-around ${inter700.className}`}
           >
-            <div className="flex flex-col items-end mr-4 gap-1 mt-4">
-              <div>
-                <Button
-                  className={`bg-lightGreen rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9
-                 border-darkBlue hover:shadow-inner hover:shadow-green-400`}
-                  onClick={openModal}
-                >
-                  Editar Perfil
-                </Button>
-                {isModalOpen && (
-                  <ModalEditarPerfil
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                  />
-                )}
-              </div>
-              <Button
-                className={`bg-red rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9 
-                 border-darkBlue hover:shadow-inner hover:shadow-rose-400`}
-              >
-                Excluir Perfil
-              </Button>
-            </div>
             <div className="flex flex-col ml-12 gap-2 sm:ml-2">
               <h1 className="text-darkBlue text-xl sm:text-lg lg:text-2xl">
                 {professor?.nome}
--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -6,22 +6,21 @@ import { useEffect, useState } from "react";
 import { Button } from "@headlessui/react";
 import Image from "next/image";
 import Link from "next/link";
-
 import Header from "../../components/Header";
 import { inter400, inter700, inter800 } from "../../fonts/fonts";
-import "../../../globals.css";
+import "../../globals.css";
 import fotoPerfil from "../../../../public/imagens/perfil.png";
 import Publicacao from "../../components/Publicacao";
 import ModalEditarPerfil from "../../components/ModalPerfil/ModalEditarPerfil";
 import { CircleArrowLeft, Building, Mail, Dot } from "lucide-react";
 import api from "@/utils/api";
+import { useAuth } from "@/app/context/authContext";
+import HeaderDeslogado from "@/app/components/HeaderDeslogado";
 
 export default function PerfilPage() {
-  const router = useRouter();
-  const { id } = router.query; // Obtém o ID dinâmico da rota
-
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [avaliacao, setAvaliacao] = useState<any[]>([]);
+  const { userId, isAuthenticated } = useAuth();
   const [userInfo, setUserInfo] = useState({
     nome: "",
     email: "",
@@ -33,11 +32,11 @@ export default function PerfilPage() {
   const closeModal = () => setIsModalOpen(false);
 
   useEffect(() => {
-    if (id) {
-      fetchUserInfo(id as string);
-      fetchAvaliacoes(id as string);
+    if (userId) {
+      fetchUserInfo(userId as string);
+      fetchAvaliacoes(userId as string);
     }
-  }, [id]);
+  }, [userId]);
 
   const fetchUserInfo = async (userId: string) => {
     try {
@@ -51,7 +50,8 @@ export default function PerfilPage() {
 
   const fetchAvaliacoes = async (userId: string) => {
     try {
-      const response = await api.get(`/user/${userId}/avaliacoes`);
+      const response = await api.get(`/avaliacoes/user/${Number(userId)}`);
+      console.log(response.data);
       setAvaliacao([...response.data]);
     } catch (err) {
       console.error("Erro ao buscar avaliações:", err);
@@ -60,7 +60,7 @@ export default function PerfilPage() {
 
   return (
     <div className="bg-background flex flex-col justify-center items-center h-full w-screen relative">
-      <Header />
+      {isAuthenticated ? <Header /> : <HeaderDeslogado />}
       <div className="flex items-center w-full h-[30px]">
         <Button className="focus:outline-none rounded-full hover:bg-emerald-300 ml-20">
           <Link href="/feed-logado">
@@ -81,27 +81,31 @@ export default function PerfilPage() {
             className={`bg-white h-auto border-solid border-b-2 w-full flex flex-col justify-around ${inter700.className}`}
           >
             <div className="flex flex-col items-end mr-4 gap-1 mt-4">
-              <div>
-                <Button
-                  className={`bg-lightGreen rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9
+              {isAuthenticated && (
+                <>
+                  <div>
+                    <Button
+                      className={`bg-lightGreen rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9
                  border-darkBlue hover:shadow-inner hover:shadow-green-400`}
-                  onClick={openModal}
-                >
-                  Editar Perfil
-                </Button>
-                {isModalOpen && (
-                  <ModalEditarPerfil
-                    isOpen={isModalOpen}
-                    onClose={closeModal}
-                  />
-                )}
-              </div>
-              <Button
-                className={`bg-red rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9 
+                      onClick={openModal}
+                    >
+                      Editar Perfil
+                    </Button>
+                    {isModalOpen && (
+                      <ModalEditarPerfil
+                        isOpen={isModalOpen}
+                        onClose={closeModal}
+                      />
+                    )}
+                  </div>
+                  <Button
+                    className={`bg-red rounded-full ${inter400.className} text-darkBlue border-2 w-36 h-9 
                  border-darkBlue hover:shadow-inner hover:shadow-rose-400`}
-              >
-                Excluir Perfil
-              </Button>
+                  >
+                    Excluir Perfil
+                  </Button>
+                </>
+              )}
             </div>
             <div className="flex flex-col ml-12 gap-2 sm:ml-2">
               <h1 className="text-darkBlue text-xl sm:text-lg lg:text-2xl">
@@ -131,10 +135,10 @@ export default function PerfilPage() {
               id={avaliacao.id}
               createdAt={avaliacao.createdAt}
               usuarioId={avaliacao.usuarioId}
-              professor={avaliacao.professor.nome}
+              professor={avaliacao?.professor?.nome}
               updatedAt={avaliacao.updatedAt}
-              disciplina={avaliacao.diciplina.nome}
-              usuario={avaliacao.usuario.nome}
+              disciplina={avaliacao?.diciplina?.nome}
+              usuario={avaliacao?.usuario?.nome}
             />
           ))}
         </div>
